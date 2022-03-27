d9858c243c3ab21790e4ec8889d2ee1ad124b754
keyurmistry21 <keyur@engineering.com>
2022-03-27 18:30:45 -0400

    One variable declaration

---
+++ 02_variable_declaration/01_one_variable_declaration/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var message string
+	message = "Welcome to Keyur mistry Github !!"
+
+	fmt.Printf("%v \n", message)
+
+	fmt.Printf("message(type) - %T | (value) - %v \n", message, message)
+}
